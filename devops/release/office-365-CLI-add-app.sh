3aca1fe9deb4b1da06b9afbfcce354d80c95391b
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-21 14:01:56 +0530

    Revert Back Changes

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -1,13 +1,12 @@
 SITE_URL=$1
 EMAIL=$2
 PASS=$3
-SCOPE=$4
 
 npm i  -g @pnp/office365-cli
 o365 version
 
-o365 spo login $SITE_URL --authType password --userName $EMAIL --password  $PASS 
+o365 spo login $SITE_URL --authType password --userName $EMAIL --password  $PASS
 
-o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg" --appCatalogUrl $SITE_URL --scope $SCOPE --overwrite
+o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg" --overwrite
 
-o365 spo app deploy --name dev-ops-cicd.sppkg --appCatalogUrl $SITE_URL --scope $SCOPE --skipFeactureDeployment
+o365 spo app deploy --name dev-ops-cicd.sppkg --skipFeactureDeployment
