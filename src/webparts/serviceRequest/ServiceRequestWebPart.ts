cfa148374070806afd6256b2a05832145cd2bc69
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-21 11:12:50 +0530

    initial commit

---
+++ src/webparts/serviceRequest/ServiceRequestWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'ServiceRequestWebPartStrings';
+import ServiceRequest from './components/ServiceRequest';
+import { IServiceRequestProps } from './components/IServiceRequestProps';
+
+export interface IServiceRequestWebPartProps {
+  description: string;
+}
+
+export default class ServiceRequestWebPart extends BaseClientSideWebPart<IServiceRequestWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IServiceRequestProps > = React.createElement(
+      ServiceRequest,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ src/webparts/serviceRequest/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IServiceRequestWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ServiceRequestWebPartStrings' {
+  const strings: IServiceRequestWebPartStrings;
+  export = strings;
+}
