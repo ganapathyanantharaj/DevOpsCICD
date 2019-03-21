18d6bab320d47ab94f3639ea5ca7df032ed0327f
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-21 11:44:26 +0530

    office 365 cli

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -7,6 +7,6 @@ o365 version
 
 o365 spo login $SITE_URL --authType password --userName $EMAIL --password  $PASS 
 
-o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/demowebpart.sppkg"  --overwrite
+o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg"  --overwrite
 
-o365 spo app deploy --name demowebpart.sppkg --skipFeactureDeployment
+o365 spo app deploy --name dev-ops-cicd.sppkg --skipFeactureDeployment
