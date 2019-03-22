410eb1876123cd851bc9beec8a6e7638dc45041c
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-22 17:26:58 +0530

    Testing

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -9,4 +9,4 @@ o365 spo login $SITE_URL --authType password --userName $EMAIL --password  $PASS
 
 o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg" --overwrite
 
-o365 spo app deploy --name dev-ops-cicd.sppkg --skipFeactureDeployment
+o365 spo app deploy --name dev-ops-cicd.sppkg --skipFeatureDeployment
