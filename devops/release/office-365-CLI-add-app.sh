9d73bd5f2ca766b491eb084c1ead26d0abdebe82
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-22 18:19:17 +0530

    Hi

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -2,11 +2,11 @@ SITE_URL='https://developertrail.sharepoint.com'
 EMAIL='ganapathy.anantharaj@developertrail.onmicrosoft.com'
 PASS='Developer$@123'
 
-npm i  -g @pnp/office365-cli
+npm i -g @pnp/office365-cli
 o365 version
 
-o365 spo login $SITE_URL --authType password --userName $EMAIL --password  $PASS
+o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
 
-o365 spo app add --filePath  "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg" --overwrite
+o365 spo app add --filePath "./_SPFx build/SPFx sppkg/dev-ops-cicd.sppkg" --overwrite
 
 o365 spo app deploy --name dev-ops-cicd.sppkg --skipFeatureDeployment
