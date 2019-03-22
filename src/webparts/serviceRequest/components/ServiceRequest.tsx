468151ec5e35bb07bb6a64db4af8816c1e360482
ganapathyanantharaj <48473993+ganapathyanantharaj@users.noreply.github.com>
2019-03-22 17:40:59 +0530

    Testing

--- src/webparts/serviceRequest/components/ServiceRequest.tsx
+++ src/webparts/serviceRequest/components/ServiceRequest.tsx
@@ -10,7 +10,7 @@ export default class ServiceRequest extends React.Component<IServiceRequestProps
         <div className={ styles.container }>
           <div className={ styles.row }>
             <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to Genpact -- Service Request</span>
+              <span className={ styles.title }>Welcome to Genpact ---- Service Request</span>
               <p className={ styles.subTitle }>Customize Service Request Using SharePoint Web Parts</p>
               <p className={ styles.description }>{escape(this.props.description)}</p>
               <a href="https://aka.ms/spfx" className={ styles.button }>
